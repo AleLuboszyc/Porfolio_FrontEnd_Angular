67e2ada1bb5459c9fa52524c8dca36302ae97e64
AleLuboszyc <martinaleluboszyc@gmail.com>
2022-11-01 13:48:30 -0300

    Subiendo Porfolio Argentina Programa FrontEnd Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { EstudiosComponent } from './componentes/estudios/estudios.component';
+import { CertificadosComponent } from './componentes/certificados/certificados.component';
+import { PracticasComponent } from './componentes/practicas/practicas.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    EstudiosComponent,
+    CertificadosComponent,
+    PracticasComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
